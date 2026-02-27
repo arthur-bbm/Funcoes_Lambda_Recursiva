123e2af8fbe9a9cd39fb934ffa9cbe84ae7683c3
arthur.bbm <arthur.castro@ort.org.br>
2026-02-27 15:12:54 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- funcoes_lambda.py
+++ funcoes_lambda.py
@@ -1,4 +1,19 @@
+#Funcao para reajustar o valor para 130%
+
 def calculo_reajuste(valor):
     return valor * 1,3
 
 print(calculo_reajuste(1000))
+
+
+#Funcao lambda - reajuste
+
+reajuste = lambda x: x * 1,3
+
+print(reajuste(1000))
+
+#Funcao lambda - maiuscula
+
+maiuscula = lambda string: string.upper()
+
+print(maiuscula("leonardo"))
