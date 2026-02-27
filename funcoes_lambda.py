8838f484e01453cae67f5e058abc0c66f52da3b9
arthur.bbm <arthur.castro@ort.org.br>
2026-02-27 15:29:41 -0300

    resto dos lambdas

--- funcoes_lambda.py
+++ funcoes_lambda.py
@@ -17,3 +17,12 @@ print(reajuste(1000))
 maiuscula = lambda string: string.upper()
 
 print(maiuscula("leonardo"))
+
+lista = ['ana', 'joao', 'maria', 'carlos']
+
+print(list(map(maiuscula, lista)))
+
+
+numros = [1,2,3,4,5,6,7,8,9,10]
+
+print(list(map(lambda x: x % 2 == 0, numros)))
